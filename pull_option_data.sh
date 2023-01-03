d9869dc1339bb78bbb9b952446152b88c8cbc6f9
mvellayan <muthu.vellayan@gmail.com>
2023-01-03 14:02:17 -0500

    renamed execution script

--- pull_option_data.sh
+++ pull_option_data.sh
@@ -1,7 +1,8 @@
-cd ~/Development/OptionList7/
-source ./bin/activate
-cd ~/Development/OptionList7/data-collection
+#cd ~/Development/OptionList7/
+#source ./bin/activate
 echo `date`: Step: 1
+cd ./data-collection
+echo `date`: Step: 2
 python3 1dc-pull-option-list.py
 echo `date`: Step: 3 
 python3 3dc-plan-tasks.py
@@ -9,10 +10,9 @@ echo `date`: step: 4
 python3  4dc-execute-tasks.py
 
 
-cd ~/Development/OptionList7/data-prep/
+cd ../data-prep/
 echo `date`: step: 5
 python3 1p-project-join.py	
 echo `date`: step: 6
 python3 2p-load-to-mysql.py
 echo `date`: Done!
-cd ~/Development/OptionList7/
