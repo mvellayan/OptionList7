2871eabb472f25482f6e865c0ec24275f8122fb6
mvellayan <muthu.vellayan@gmail.com>
2023-01-03 14:03:17 -0500

    - connecting to 'host.docker.internal' host to support docker connections - checking for 300 or more quotes to consider a day as complete

--- common/ol_ib.py
+++ common/ol_ib.py
@@ -16,7 +16,7 @@ global_ib = IB()
 def getIB():
     global global_ib
     if not global_ib.isConnected():
-        global_ib.connect('127.0.0.1', 7496, clientId=1)
+        global_ib.connect('host.docker.internal', 7496, clientId=1)
     return global_ib
 
 """
@@ -45,26 +45,27 @@ def check_pull_historical_quote_to_file(sDate:str, contract):
         fn = olc.DATA_DIR + olu.getYear(sDate) + "/" + olu.getMonth(sDate) + "/" + olu.getDay(sDate) + "/sq-" + sq_type + "-" + sym + ".csv"
         if os.path.exists(fn):
             df = pd.read_csv(fn, index_col=None)
-        else:
-            ib = getIB()
-            bars = ib.reqHistoricalData(contract, endDateTime=sDate + " 16:00:00",
-                                        durationStr='5 D', barSizeSetting='1 min',
-                                        whatToShow=sq_type, useRTH=True)
-            if len(bars) > 0:
-                df = util.df(bars)
-                df['symbol'] = contract.symbol
-                df['localSymbol'] = contract.localSymbol
-                df['conId'] = contract.conId
-                # get unique dates in df
+            if df.shape[0] > 300:
+                continue
+        ib = getIB()
+        bars = ib.reqHistoricalData(contract, endDateTime=sDate + " 16:00:00",
+                                    durationStr='5 D', barSizeSetting='1 min',
+                                    whatToShow=sq_type, useRTH=True)
+        if len(bars) > 0:
+            df = util.df(bars)
+            df['symbol'] = contract.symbol
+            df['localSymbol'] = contract.localSymbol
+            df['conId'] = contract.conId
+            # get unique dates in df
 
-                dates = df['date'][::].astype(str).str.slice(stop=10).unique()
-                # for each date, filter rows & save
-                for date_ in dates:
-                    fn2 = olc.DATA_DIR + olu.getYear(date_) + "/" + olu.getMonth(date_) + "/" + olu.getDay(date_) + "/sq-" + sq_type + "-" + sym + ".csv"
-                    Path(fn2).parent.mkdir(parents=True, exist_ok=True)
-                    df_ = df.loc[df['date'][::].astype(str).str.slice(stop=10) == date_]
-                    df_.to_csv(fn2, index=False)
-            else:
-                df = pd.DataFrame()
-                break
+            dates = df['date'][::].astype(str).str.slice(stop=10).unique()
+            # for each date, filter rows & save
+            for date_ in dates:
+                fn2 = olc.DATA_DIR + olu.getYear(date_) + "/" + olu.getMonth(date_) + "/" + olu.getDay(date_) + "/sq-" + sq_type + "-" + sym + ".csv"
+                Path(fn2).parent.mkdir(parents=True, exist_ok=True)
+                df_ = df.loc[df['date'][::].astype(str).str.slice(stop=10) == date_]
+                df_.to_csv(fn2, index=False)
+        else:
+            df = pd.DataFrame()
+            break
     return df
